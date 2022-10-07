3d5c8fb569084cfb35a1fe43592f87c9df280496
mikeqiu2011 <mikeqiu2011@163.com>
2022-10-07 20:04:01 +0800

    current_user.is_authenticated to redirect to home

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,7 +1,9 @@
 from flask import render_template, url_for, flash, redirect
 from flaskblog import app, db, bcrypt
 from flaskblog.forms import RegistrationForm, LoginForm
-from flaskblog.models import User, Post
+from flaskblog.models import User
+from flask_login import login_user, current_user
+
 
 posts = [
     {
@@ -31,6 +33,8 @@ def about():
 
 @app.route("/register", methods=['GET', 'POST'])
 def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = RegistrationForm()
     if form.validate_on_submit():
         hashed_password = bcrypt.generate_password_hash(
@@ -48,12 +52,18 @@ def register():
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = LoginForm()
     if form.validate_on_submit():
-        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, \
+                form.password.data):
+            login_user(user, remember=form.remember.data)
             flash('You have been logged in!', 'success')
             return redirect(url_for('home'))
         else:
-            flash('Login Unsuccessful. Please check username and password', 'danger')
+            flash('Login Unsuccessful. Please check email and password',
+                  'danger')
     return render_template('login.html', title='Login', form=form)
 
