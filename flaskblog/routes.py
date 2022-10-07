e0dee952a26ef686ff84130edb35aa94d49de703
mikeqiu2011 <mikeqiu2011@163.com>
2022-10-07 20:12:05 +0800

    dynamic show logout button

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -2,8 +2,7 @@ from flask import render_template, url_for, flash, redirect
 from flaskblog import app, db, bcrypt
 from flaskblog.forms import RegistrationForm, LoginForm
 from flaskblog.models import User
-from flask_login import login_user, current_user
-
+from flask_login import login_user, current_user, logout_user
 
 posts = [
     {
@@ -19,6 +18,7 @@ posts = [
         'date_posted': 'April 21, 2018'
     }
 ]
+
 
 @app.route("/")
 @app.route("/home")
@@ -58,7 +58,7 @@ def login():
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
         if user and bcrypt.check_password_hash(user.password, \
-                form.password.data):
+                                               form.password.data):
             login_user(user, remember=form.remember.data)
             flash('You have been logged in!', 'success')
             return redirect(url_for('home'))
@@ -67,3 +67,8 @@ def login():
                   'danger')
     return render_template('login.html', title='Login', form=form)
 
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
