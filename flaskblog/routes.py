4fba6fba645a6d201bdd986820e8cd84c6732701
mikeqiu2011 <mikeqiu2011@163.com>
2022-10-07 23:53:36 +0800

    now can update post

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, url_for, flash, redirect, request
+from flask import render_template, url_for, flash, redirect, request, abort
 from flaskblog import app, db, bcrypt
 from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm, \
     PostForm
@@ -122,11 +122,39 @@ def new_post():
         flash(f'posted!', 'success')
         return redirect(url_for('home'))
 
-    return render_template('create_post.html', title='New Post', form=form)
+    return render_template('create_post.html',
+                           title='New Post',
+                           form=form,
+                           legend='New Post')
 
 
-@app.route('/port/<int:post_id>', methods=['GET', 'POST'])
+@app.route('/post/<int:post_id>')
 @login_required
 def post(post_id):
     post = Post.query.get_or_404(post_id)
     return render_template('post.html', title=post.title, post=post)
+
+
+@app.route('/post/<int:post_id>/update', methods=['GET', 'POST'])
+@login_required
+def update_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+
+    form = PostForm()
+    if form.validate_on_submit():
+        post.title = form.title.data
+        post.content = form.content.data
+        # db.session.add(post) # no need to add again, as already in database
+        db.session.commit()
+        flash('post updated!', 'success')
+        return redirect(url_for('post', post_id=post.id))
+
+    form.title.data = post.title
+    form.content.data = post.content
+
+    return render_template('create_post.html',
+                           title='Update Post',
+                           form=form,
+                           legend='Update Post')
