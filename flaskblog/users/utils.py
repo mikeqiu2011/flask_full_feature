8e7d94bc8d6be56f8458db97f04515e7785c92c4
mikeqiu2011 <mikeqiu2011@163.com>
2022-10-10 15:51:44 +0800

    using config class and create_app

--- flaskblog/users/utils.py
+++ flaskblog/users/utils.py
@@ -1,14 +1,14 @@
 import os
 import secrets
 from PIL import Image
-from flaskblog import app
+from flask import current_app
 
 
 def save_picture(form_picture):
     random_hex = secrets.token_hex(8)
     _, f_ext = os.path.splitext(form_picture.filename)
     picture_fn = random_hex + f_ext
-    picture_path = os.path.join(app.root_path, 'static/profile_pics',
+    picture_path = os.path.join(current_app.root_path, 'static/profile_pics',
                                 picture_fn)
 
     # resize the picture
